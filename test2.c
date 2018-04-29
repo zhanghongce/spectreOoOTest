849e66d3f7a094fbb62ac578c477ab7656c2fc9b
Hongce Zhang <hongcez@princeton.edu>
2018-04-29 16:41:29 -0400

    in test2

---
+++ test2.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+__inline__ void clflush(volatile void *p)
+{
+    asm volatile ("clflush (%0)" :: "r"(p));
+}
+
+__inline__ uint64_t rdtsc()
+{
+    unsigned long a, d;
+    asm volatile ("cpuid; rdtsc" : "=a" (a), "=d" (d) : : "ebx", "ecx");
+    return a | ((uint64_t)d << 32);
+}
+
+unsigned char *table6;
+unsigned char *table1;
+unsigned char table2[64];
+unsigned char table3[64];
+unsigned char table4[64];
+unsigned char *table5;
+
+const int REPEATCNT = 100;
+
+
+ __inline__ unsigned long probe(char *adrs) {
+	volatile unsigned long time;
+	
+	asm __volatile__ (
+		" mfence 			\n"
+		" lfence			\n"
+		" rdtsc				\n"
+		" lfence			\n"
+		" movl %%eax, %%esi \n"
+		" movl (%1) , %%eax \n"
+		" lfence 			\n"
+		" rdtsc				\n"
+		" subl %%esi, %%eax \n"
+		" clflush 0(%1)     \n"
+		: "=a" (time)
+		: "c" (adrs)
+		: "%esi","%edx"
+	);
+	return time;
+}
+
+int main()
+{
+	
+	unsigned char x;
+	unsigned char y = rand()%256;
+	int idx;
+	volatile unsigned long t0;
+	volatile unsigned long t1;
+	volatile unsigned long t2;
+	volatile unsigned long tt;
+	volatile unsigned long tb5_1;
+	volatile unsigned long tb5_2;
+	volatile unsigned long tb6_acc;
+	int both_unfetch = 0;
+	int fetch_old = 0;
+	int OoO = 0;
+	unsigned char det = rand()%256;
+	int u = rand() % 256;
+	int v;
+
+	
+	table1 = malloc(sizeof(unsigned char *)*64);
+	table5 = malloc(sizeof(unsigned char *)*64);
+	table6 = malloc(sizeof(unsigned char *)*64);
+	
+	for (idx = 0; idx < 64; idx ++ ) {
+		table1[idx] = rand()%256;
+		table2[idx] = rand()%256;
+		table3[idx] = rand()%256;
+		table4[idx] = rand()%256;
+		table5[idx] = rand()%256;
+		table6[idx] = rand()%256;
+		}
+		
+	probe(table2);
+	probe(table3);
+	probe(table4);
+	t0 = probe(table1);	 // clear table1 cache
+	tb5_1 = probe(table5); // 
+	
+	for(idx = 0; idx < 1000; idx ++)
+	{
+		y = rand() % 256;
+		x = rand() % 256;
+		t1 = probe(table1);
+		//tt = probe(table1);
+		probe(table2);
+		probe(table3);
+		probe(table4); // make sure the following loads will miss
+		t0 = probe(table5); // tb5 
+		//t0 = probe(table5);
+		probe(table6);
+		
+		x  = table4[x%64];
+		x  = table3[x%64];
+		x  = table2[x%64];
+		if(x < 100)
+		{
+			u *= u;
+			u *= u;
+			u = *((unsigned int *)table1 + u%16 );
+			v = *((unsigned int *)table5 );
+			
+			
+		}
+		t2 = probe(table1);
+		tb5_2 = probe(table5); 
+		tb6_acc = probe(table6);
+		if( x>= 100 && t2 > 200 && tb5_2 < 100  )
+			printf("T1 before this rnd: %ld,T1 after rnd: %ld,T5 before: %ld, T5 before rnd: %ld, T5 after rnd:%ld\n",t1,t2,tb5_1,t0,tb5_2);
+
+	}
+	
+   free(table1);
+   free(table5);
+   free(table6);
+   return u+table1[0]+v;
+
+};
+
+/*
+int main()
+{
+	uint64_t msr;
+	uint64_t msr2;
+	unsigned char x;
+	unsigned char y = rand()%256;
+	
+	int idx;
+	int repeat;
+	double t1avg = 0;
+	double t2avg = 0;
+	int ret = 0;
+	
+	for (idx = 0; idx < 64; idx ++ )
+		table1[idx] = rand()%256;
+	
+    
+    for (repeat = 0; repeat < REPEATCNT; repeat ++ )
+    {
+		clflush(table1);
+		//table1[1] = table1[2];
+		msr = rdtsc();
+		x = table1[y];
+		msr2 = rdtsc();
+		ret += x;
+		t1avg += msr2-msr;
+	}
+	
+	for (repeat = 0; repeat < REPEATCNT; repeat ++ )
+    {
+		clflush(table1);
+		table1[1] = table1[2];
+		msr = rdtsc();
+		x = table1[y];
+		msr2 = rdtsc();
+		ret += x;
+		t2avg += msr2-msr;
+	}
+   					
+   printf("%lf,%lf\n", t1avg/REPEATCNT, t2avg/REPEATCNT);
+   return ret;
+}
+*/
+
+/*
+*/
+/*
+
+{
+	int idx = 0;
+	int rand = 
+	for (; idx < 100; idx ++)
+	{	// let's abuse the branch predictor to predict taken
+		
+		if( idx < 99 )
+		{
+			mul %%eax
+			mul %%eax
+			mov (%1,%%eax),%%edx
+			mov (%2),
+			
+		}	
+	}
+}*/
