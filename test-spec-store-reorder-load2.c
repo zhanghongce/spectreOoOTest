98b13e7355919a03c8c99097739118863c621788
Hongce Zhang <hongcez@princeton.edu>
2018-10-12 11:24:16 -0400

    test l-s-l reorder

---
+++ test-spec-store-reorder-load2.c
@@ -0,0 +1,287 @@
+// Hongce Zhang @ Princeton
+// The way to rule out prefetcher effects is shown in test2.c
+// In this test, we will discuss 
+// whether speculative store is usable
+// Please compile it with -O0 
+// use g++ (Ubuntu 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609 please
+
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+__inline__ void clflush(volatile void *p)
+{
+    asm volatile ("clflush (%0)" :: "r"(p));
+}
+
+__inline__ uint64_t rdtsc()
+{
+    unsigned long a, d;
+    asm volatile ("cpuid; rdtsc" : "=a" (a), "=d" (d) : : "ebx", "ecx");
+    return a | ((uint64_t)d << 32);
+}
+
+unsigned char *table0;
+unsigned char *table_sep; // used as a separator
+unsigned char *table6;    // used as a checker (for prefetch)
+unsigned char *tableCheck; // used to see if the write value is used
+unsigned char *table1;
+unsigned char table2[64];
+unsigned char table3[64];
+unsigned char table4[64];
+unsigned char *table5;
+
+const int REPEATCNT = 100;
+# define CACHE_HIT 100
+# define CACHE_MISS 100
+
+
+ __inline__ unsigned long probe(unsigned char *adrs) {
+	volatile unsigned long time;
+	
+	asm __volatile__ (
+		" mfence 			\n"
+		" lfence			\n"
+		" rdtsc				\n"
+		" lfence			\n"
+		" movl %%eax, %%esi \n"
+		" movl (%1) , %%eax \n"
+		" lfence 			\n"
+		" rdtsc				\n"
+		" subl %%esi, %%eax \n"
+		" clflush 0(%1)     \n"
+		: "=a" (time)
+		: "c" (adrs)
+		: "%esi","%edx"
+	);
+	return time;
+}
+
+int main()
+{
+	
+	unsigned char x;
+	unsigned char y = rand()%256;
+	int idx;
+	volatile unsigned long t0;
+	volatile unsigned long t1;
+	volatile unsigned long t2;
+	volatile unsigned long tt;
+	volatile unsigned long tb5_1;
+	volatile unsigned long tb5_2;
+	volatile unsigned long tb6_acc;
+	volatile unsigned long tc[4];
+    volatile unsigned long tb0_acc;
+	int both_unfetch = 0;
+	int fetch_old = 0;
+	int OoO = 0;
+	unsigned char det = rand()%256;
+	int u = rand() % 256;
+	int v = rand() % 4; // we only test 4
+    unsigned qi = rand() % 256;
+	int w;
+	int ww;
+	volatile unsigned long tb6_acc_prefetcher_effect = 0;
+	volatile unsigned long non_prefetched_load_tb1_cnt = 0;
+	volatile unsigned long table_is_used = 0;
+	volatile unsigned long table_is_not_used = 0;
+    volatile unsigned long table0_not_accessed_while_later_reordered = 0;
+	
+    table0    = (unsigned char *) malloc(sizeof(unsigned char )*64);
+	table1    = (unsigned char *) malloc(sizeof(unsigned char )*64);
+	table_sep = (unsigned char *) malloc(sizeof(unsigned char )*64*4);
+	table6    = (unsigned char *) malloc(sizeof(unsigned char )*64);
+	table5    = (unsigned char *) malloc(sizeof(unsigned char )*64);
+	tableCheck= (unsigned char *) malloc(sizeof(unsigned char )*64*4);
+	
+	for (idx = 0; idx < 64; idx ++ ) {
+		table1[idx] = rand()%256;
+		table2[idx] = rand()%256;
+		table3[idx] = rand()%256;
+		table4[idx] = rand()%256;
+		table5[idx] = rand()%256;
+		table6[idx] = rand()%256;
+		}
+		
+	probe(table2);
+	probe(table3);
+	probe(table4);
+	t0 = probe(table1);	 // clear table1 cache
+	tb5_1 = probe(table5); // 
+	
+	for(idx = 0; idx < 100000; idx ++)
+	{
+		y = rand() % 256;
+		x = rand() % 256;
+		v = rand() % 4;
+        qi = rand() % 256;
+		t1 = probe(table1);
+		//tt = probe(table1);
+		probe(table2);
+		probe(table3);
+		probe(table4); // make sure the following loads will miss
+		t0 = probe(table5); // tb5 
+		//t0 = probe(table5);
+		probe(table6);
+		probe(table_sep);
+		probe(table_sep+64);
+		probe(table_sep+64*2);
+		probe(table_sep+64*3);
+		probe(tableCheck);
+		probe(tableCheck+64);
+		probe(tableCheck+64*2);
+		probe(tableCheck+64*3);
+        probe(table0);
+		
+		x  = table4[x%64];
+		x  = table3[x%64];
+		x  = table2[x%64];
+		if(x < 100)
+		{
+			qi *= qi;
+			qi *= qi;
+            qi *= qi;
+			qi = *((unsigned int *)table0 + qi%16 );
+			// v = *((unsigned int *)table5 );  // *** Line 1 *** //
+			// v = *((unsigned int *)table1 + u%16 );  // this is read
+			*((unsigned int *)table1 + u%16 ) = v; // make sure it will not use v directly
+			w = *((unsigned int *)table1 + u%16 ); // w = 0..3
+			ww = *(((unsigned int *)tableCheck)+ w * 16); // ww
+			
+		}
+
+		t2 = probe(table1);
+		tb5_2 = probe(table5); 
+		tb6_acc = probe(table6);
+        tb0_acc = probe(table0);
+
+		tc[0] = probe(tableCheck+64*0);
+		tc[1] = probe(tableCheck+64*1);
+		tc[2] = probe(tableCheck+64*2);
+		tc[3] = probe(tableCheck+64*3);
+
+		if( x>= 100 && t2 < CACHE_HIT && tb5_2 > CACHE_MISS  && tb6_acc > CACHE_MISS ) {
+			//printf("%ld,%ld,%ld,%ld\n",t1,t2,tb5_1,tb5_2);
+			non_prefetched_load_tb1_cnt ++;
+		}
+		if( tb6_acc < CACHE_HIT) {
+			//printf("%ld , tb6 acc!\n", tb6_acc);
+			tb6_acc_prefetcher_effect ++;
+		}
+
+        if ( x>= 100 && t2 < CACHE_HIT && tb5_2 > CACHE_MISS  && tb6_acc > CACHE_MISS && tb0_acc > CACHE_MISS) {
+            table0_not_accessed_while_later_reordered ++ ;
+        }
+
+		if( x>=100 && t2 < CACHE_HIT && tb5_2 > CACHE_MISS  && tb6_acc > CACHE_MISS &&
+			( (tc[0] < CACHE_HIT) || (tc[1] < CACHE_HIT) || (tc[2] < CACHE_HIT) || (tc[3] < CACHE_HIT)  )
+			&& (!( (tc[0] < CACHE_HIT && tc[1] < CACHE_HIT) ||
+				   (tc[0] < CACHE_HIT && tc[2] < CACHE_HIT)	||
+				   (tc[0] < CACHE_HIT && tc[3] < CACHE_HIT)	||
+				   (tc[1] < CACHE_HIT && tc[2] < CACHE_HIT)	||
+				   (tc[1] < CACHE_HIT && tc[3] < CACHE_HIT)	||
+				   (tc[2] < CACHE_HIT && tc[3] < CACHE_HIT)	))
+			&& !( (tc[0] < CACHE_HIT && tc[1] < CACHE_HIT && tc[2] < CACHE_HIT ) ||
+				  (tc[0] < CACHE_HIT && tc[1] < CACHE_HIT && tc[3] < CACHE_HIT ) ||
+				  (tc[0] < CACHE_HIT && tc[2] < CACHE_HIT && tc[3] < CACHE_HIT ) ||
+				  (tc[1] < CACHE_HIT && tc[2] < CACHE_HIT && tc[3] < CACHE_HIT ) )
+			&& ! (tc[0] < CACHE_HIT && tc[1] < CACHE_HIT && tc[2] < CACHE_HIT && tc[3] < CACHE_HIT )
+
+				    ) {
+
+			bool check = true;
+			for(int tp = 0; tp < 4; tp ++) {
+				if ( tp == v && !( tc [tp] < CACHE_HIT) ) check = false;
+				if ( tp != v && !( tc [tp] > CACHE_MISS)) check = false;
+			}
+			printf("v: %d, Time = %ld, %ld, %ld, %ld,  %c\n", v, tc[0], tc[1], tc[2], tc[3], check?'*':' ' );
+			if (check) {
+				table_is_used ++;
+            }
+			else
+				table_is_not_used++;
+		}
+
+	}
+	
+   free(table1);
+   free(table5);
+   free(table6);
+   free(table_sep);
+   free(table0);
+   printf("Non-prefetched tb1 load: %ld\n", non_prefetched_load_tb1_cnt );
+   printf("used: %ld vs. unused:%ld\n", table_is_used, table_is_not_used);
+   printf("store-load at least touched, but tb0 not touched:%ld\n", table0_not_accessed_while_later_reordered);
+   printf("prefetcher effect: %ld\n", tb6_acc_prefetcher_effect);
+   return u+v+ww+qi;
+
+};
+
+/*
+int main()
+{
+	uint64_t msr;
+	uint64_t msr2;
+	unsigned char x;
+	unsigned char y = rand()%256;
+	
+	int idx;
+	int repeat;
+	double t1avg = 0;
+	double t2avg = 0;
+	int ret = 0;
+	
+	for (idx = 0; idx < 64; idx ++ )
+		table1[idx] = rand()%256;
+	
+    
+    for (repeat = 0; repeat < REPEATCNT; repeat ++ )
+    {
+		clflush(table1);
+		//table1[1] = table1[2];
+		msr = rdtsc();
+		x = table1[y];
+		msr2 = rdtsc();
+		ret += x;
+		t1avg += msr2-msr;
+	}
+	
+	for (repeat = 0; repeat < REPEATCNT; repeat ++ )
+    {
+		clflush(table1);
+		table1[1] = table1[2];
+		msr = rdtsc();
+		x = table1[y];
+		msr2 = rdtsc();
+		ret += x;
+		t2avg += msr2-msr;
+	}
+   					
+   printf("%lf,%lf\n", t1avg/REPEATCNT, t2avg/REPEATCNT);
+   return ret;
+}
+*/
+
+/*
+*/
+/*
+
+{
+	int idx = 0;
+	int rand = 
+	for (; idx < 100; idx ++)
+	{	// let's abuse the branch predictor to predict taken
+		
+		if( idx < 99 )
+		{
+			mul %%eax
+			mul %%eax
+			mov (%1,%%eax),%%edx
+			mov (%2),
+			
+		}	
+	}
+}*/
---
+++ test-spec-store-usable.s
@@ -0,0 +1,735 @@
+	.file	"test-spec-store-usable.c"
+	.globl	table_sep
+	.bss
+	.align 8
+	.type	table_sep, @object
+	.size	table_sep, 8
+table_sep:
+	.zero	8
+	.globl	table6
+	.align 8
+	.type	table6, @object
+	.size	table6, 8
+table6:
+	.zero	8
+	.globl	tableCheck
+	.align 8
+	.type	tableCheck, @object
+	.size	tableCheck, 8
+tableCheck:
+	.zero	8
+	.globl	table1
+	.align 8
+	.type	table1, @object
+	.size	table1, 8
+table1:
+	.zero	8
+	.globl	table2
+	.align 32
+	.type	table2, @object
+	.size	table2, 64
+table2:
+	.zero	64
+	.globl	table3
+	.align 32
+	.type	table3, @object
+	.size	table3, 64
+table3:
+	.zero	64
+	.globl	table4
+	.align 32
+	.type	table4, @object
+	.size	table4, 64
+table4:
+	.zero	64
+	.globl	table5
+	.align 8
+	.type	table5, @object
+	.size	table5, 8
+table5:
+	.zero	8
+	.section	.rodata
+	.align 4
+	.type	_ZL9REPEATCNT, @object
+	.size	_ZL9REPEATCNT, 4
+_ZL9REPEATCNT:
+	.long	100
+	.section	.text._Z5probePh,"axG",@progbits,_Z5probePh,comdat
+	.weak	_Z5probePh
+	.type	_Z5probePh, @function
+_Z5probePh:
+.LFB4:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movq	-24(%rbp), %rax
+	movq	%rax, %rcx
+#APP
+# 58 "test-spec-store-usable.c" 1
+	 mfence 			
+ lfence			
+ rdtsc				
+ lfence			
+ movl %eax, %esi 
+ movl (%rcx) , %eax 
+ lfence 			
+ rdtsc				
+ subl %esi, %eax 
+ clflush 0(%rcx)     
+
+# 0 "" 2
+#NO_APP
+	movq	%rax, -8(%rbp)
+	movq	-8(%rbp), %rax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE4:
+	.size	_Z5probePh, .-_Z5probePh
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"v: %d, Time = %ld, %ld, %ld, %ld,  %c\n"
+.LC1:
+	.string	"Non-prefetched tb1 load: %ld\n"
+.LC2:
+	.string	"used: %ld vs. unused:%ld\n"
+.LC3:
+	.string	"prefetcher effect: %ld\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB5:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$184, %rsp
+	.cfi_offset 3, -24
+	movq	%fs:40, %rax
+	movq	%rax, -24(%rbp)
+	xorl	%eax, %eax
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movb	%al, -183(%rbp)
+	movl	$0, -164(%rbp)
+	movl	$0, -160(%rbp)
+	movl	$0, -156(%rbp)
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movb	%al, -182(%rbp)
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movl	%eax, -152(%rbp)
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$30, %eax
+	addl	%eax, %edx
+	andl	$3, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movl	%eax, -176(%rbp)
+	movq	$0, -96(%rbp)
+	movq	$0, -88(%rbp)
+	movq	$0, -80(%rbp)
+	movq	$0, -72(%rbp)
+	movl	$64, %eax
+	movq	%rax, %rdi
+	call	malloc
+	movq	%rax, table1(%rip)
+	movl	$256, %eax
+	movq	%rax, %rdi
+	call	malloc
+	movq	%rax, table_sep(%rip)
+	movl	$64, %eax
+	movq	%rax, %rdi
+	call	malloc
+	movq	%rax, table6(%rip)
+	movl	$64, %eax
+	movq	%rax, %rdi
+	call	malloc
+	movq	%rax, table5(%rip)
+	movl	$256, %eax
+	movq	%rax, %rdi
+	call	malloc
+	movq	%rax, tableCheck(%rip)
+	movl	$0, -180(%rbp)
+.L5:
+	cmpl	$63, -180(%rbp)
+	jg	.L4
+	movq	table1(%rip), %rdx
+	movl	-180(%rbp), %eax
+	cltq
+	leaq	(%rdx,%rax), %rbx
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movb	%al, (%rbx)
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movl	%eax, %edx
+	movl	-180(%rbp), %eax
+	cltq
+	movb	%dl, table2(%rax)
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movl	%eax, %edx
+	movl	-180(%rbp), %eax
+	cltq
+	movb	%dl, table3(%rax)
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movl	%eax, %edx
+	movl	-180(%rbp), %eax
+	cltq
+	movb	%dl, table4(%rax)
+	movq	table5(%rip), %rdx
+	movl	-180(%rbp), %eax
+	cltq
+	leaq	(%rdx,%rax), %rbx
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movb	%al, (%rbx)
+	movq	table6(%rip), %rdx
+	movl	-180(%rbp), %eax
+	cltq
+	leaq	(%rdx,%rax), %rbx
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movb	%al, (%rbx)
+	addl	$1, -180(%rbp)
+	jmp	.L5
+.L4:
+	movl	$table2, %edi
+	call	_Z5probePh
+	movl	$table3, %edi
+	call	_Z5probePh
+	movl	$table4, %edi
+	call	_Z5probePh
+	movq	table1(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -144(%rbp)
+	movq	table5(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -120(%rbp)
+	movl	$0, -180(%rbp)
+.L39:
+	cmpl	$99999, -180(%rbp)
+	jg	.L6
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movb	%al, -183(%rbp)
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$24, %eax
+	addl	%eax, %edx
+	movzbl	%dl, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movb	%al, -181(%rbp)
+	call	rand
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$30, %eax
+	addl	%eax, %edx
+	andl	$3, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movl	%eax, -176(%rbp)
+	movq	table1(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -136(%rbp)
+	movl	$table2, %edi
+	call	_Z5probePh
+	movl	$table3, %edi
+	call	_Z5probePh
+	movl	$table4, %edi
+	call	_Z5probePh
+	movq	table5(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -144(%rbp)
+	movq	table6(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	table_sep(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	table_sep(%rip), %rax
+	addq	$64, %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	table_sep(%rip), %rax
+	subq	$-128, %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	table_sep(%rip), %rax
+	addq	$192, %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	tableCheck(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	tableCheck(%rip), %rax
+	addq	$64, %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	tableCheck(%rip), %rax
+	subq	$-128, %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	tableCheck(%rip), %rax
+	addq	$192, %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movzbl	-181(%rbp), %eax
+	andl	$63, %eax
+	cltq
+	movzbl	table4(%rax), %eax
+	movb	%al, -181(%rbp)
+	movzbl	-181(%rbp), %eax
+	andl	$63, %eax
+	cltq
+	movzbl	table3(%rax), %eax
+	movb	%al, -181(%rbp)
+	movzbl	-181(%rbp), %eax
+	andl	$63, %eax
+	cltq
+	movzbl	table2(%rax), %eax
+	movb	%al, -181(%rbp)
+	cmpb	$99, -181(%rbp)
+	ja	.L7
+	movq	table1(%rip), %rcx
+	movl	-152(%rbp), %eax
+	cltd
+	shrl	$28, %edx
+	addl	%edx, %eax
+	andl	$15, %eax
+	subl	%edx, %eax
+	cltq
+	salq	$2, %rax
+	leaq	(%rcx,%rax), %rdx
+	movl	-176(%rbp), %eax
+	movl	%eax, (%rdx)
+	movq	table1(%rip), %rcx
+	movl	-152(%rbp), %eax
+	cltd
+	shrl	$28, %edx
+	addl	%edx, %eax
+	andl	$15, %eax
+	subl	%edx, %eax
+	cltq
+	salq	$2, %rax
+	addq	%rcx, %rax
+	movl	(%rax), %eax
+	movl	%eax, -148(%rbp)
+	movq	tableCheck(%rip), %rax
+	movl	-148(%rbp), %edx
+	movslq	%edx, %rdx
+	salq	$6, %rdx
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	movl	%eax, -172(%rbp)
+.L7:
+	movq	table1(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -128(%rbp)
+	movq	table5(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -112(%rbp)
+	movq	table6(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -104(%rbp)
+	movq	tableCheck(%rip), %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -64(%rbp)
+	movq	tableCheck(%rip), %rax
+	addq	$64, %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -56(%rbp)
+	movq	tableCheck(%rip), %rax
+	subq	$-128, %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -48(%rbp)
+	movq	tableCheck(%rip), %rax
+	addq	$192, %rax
+	movq	%rax, %rdi
+	call	_Z5probePh
+	movq	%rax, -40(%rbp)
+	cmpb	$99, -181(%rbp)
+	jbe	.L8
+	movq	-128(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L8
+	movq	-112(%rbp), %rax
+	cmpq	$100, %rax
+	jbe	.L8
+	movq	-104(%rbp), %rax
+	cmpq	$100, %rax
+	jbe	.L8
+	movl	$1, %eax
+	jmp	.L9
+.L8:
+	movl	$0, %eax
+.L9:
+	testb	%al, %al
+	je	.L10
+	movq	-88(%rbp), %rax
+	addq	$1, %rax
+	movq	%rax, -88(%rbp)
+.L10:
+	movq	-104(%rbp), %rax
+	cmpq	$99, %rax
+	setbe	%al
+	testb	%al, %al
+	je	.L11
+	movq	-96(%rbp), %rax
+	addq	$1, %rax
+	movq	%rax, -96(%rbp)
+.L11:
+	cmpb	$99, -181(%rbp)
+	jbe	.L12
+	movq	-128(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L12
+	movq	-112(%rbp), %rax
+	cmpq	$100, %rax
+	jbe	.L12
+	movq	-104(%rbp), %rax
+	cmpq	$100, %rax
+	jbe	.L12
+	movq	-64(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L13
+	movq	-56(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L13
+	movq	-48(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L13
+	movq	-40(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L12
+.L13:
+	movq	-64(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L14
+	movq	-56(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L14:
+	movq	-64(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L15
+	movq	-48(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L15:
+	movq	-64(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L16
+	movq	-40(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L16:
+	movq	-56(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L17
+	movq	-48(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L17:
+	movq	-56(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L18
+	movq	-40(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L18:
+	movq	-48(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L19
+	movq	-40(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L19:
+	movq	-64(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L20
+	movq	-56(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L20
+	movq	-48(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L20:
+	movq	-64(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L21
+	movq	-56(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L21
+	movq	-40(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L21:
+	movq	-64(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L22
+	movq	-48(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L22
+	movq	-40(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L22:
+	movq	-56(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L23
+	movq	-48(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L23
+	movq	-40(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L23:
+	movq	-64(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L24
+	movq	-56(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L24
+	movq	-48(%rbp), %rax
+	cmpq	$99, %rax
+	ja	.L24
+	movq	-40(%rbp), %rax
+	cmpq	$99, %rax
+	jbe	.L12
+.L24:
+	movl	$1, %eax
+	jmp	.L25
+.L12:
+	movl	$0, %eax
+.L25:
+	testb	%al, %al
+	je	.L26
+	movb	$1, -184(%rbp)
+	movl	$0, -168(%rbp)
+.L34:
+	cmpl	$3, -168(%rbp)
+	jg	.L27
+	movl	-168(%rbp), %eax
+	cmpl	-176(%rbp), %eax
+	jne	.L28
+	movl	-168(%rbp), %eax
+	cltq
+	movq	-64(%rbp,%rax,8), %rax
+	cmpq	$99, %rax
+	jbe	.L28
+	movl	$1, %eax
+	jmp	.L29
+.L28:
+	movl	$0, %eax
+.L29:
+	testb	%al, %al
+	je	.L30
+	movb	$0, -184(%rbp)
+.L30:
+	movl	-168(%rbp), %eax
+	cmpl	-176(%rbp), %eax
+	je	.L31
+	movl	-168(%rbp), %eax
+	cltq
+	movq	-64(%rbp,%rax,8), %rax
+	cmpq	$100, %rax
+	ja	.L31
+	movl	$1, %eax
+	jmp	.L32
+.L31:
+	movl	$0, %eax
+.L32:
+	testb	%al, %al
+	je	.L33
+	movb	$0, -184(%rbp)
+.L33:
+	addl	$1, -168(%rbp)
+	jmp	.L34
+.L27:
+	cmpb	$0, -184(%rbp)
+	je	.L35
+	movl	$42, %r8d
+	jmp	.L36
+.L35:
+	movl	$32, %r8d
+.L36:
+	movq	-40(%rbp), %rdi
+	movq	-48(%rbp), %rsi
+	movq	-56(%rbp), %rcx
+	movq	-64(%rbp), %rdx
+	movl	-176(%rbp), %eax
+	subq	$8, %rsp
+	pushq	%r8
+	movq	%rdi, %r9
+	movq	%rsi, %r8
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	addq	$16, %rsp
+	cmpb	$0, -184(%rbp)
+	je	.L37
+	movq	-80(%rbp), %rax
+	addq	$1, %rax
+	movq	%rax, -80(%rbp)
+	jmp	.L26
+.L37:
+	movq	-72(%rbp), %rax
+	addq	$1, %rax
+	movq	%rax, -72(%rbp)
+.L26:
+	addl	$1, -180(%rbp)
+	jmp	.L39
+.L6:
+	movq	table1(%rip), %rax
+	movq	%rax, %rdi
+	call	free
+	movq	table5(%rip), %rax
+	movq	%rax, %rdi
+	call	free
+	movq	table6(%rip), %rax
+	movq	%rax, %rdi
+	call	free
+	movq	table_sep(%rip), %rax
+	movq	%rax, %rdi
+	call	free
+	movq	-88(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movq	-72(%rbp), %rdx
+	movq	-80(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC2, %edi
+	movl	$0, %eax
+	call	printf
+	movq	-96(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC3, %edi
+	movl	$0, %eax
+	call	printf
+	movq	table1(%rip), %rax
+	movzbl	(%rax), %eax
+	movzbl	%al, %edx
+	movl	-152(%rbp), %eax
+	addl	%eax, %edx
+	movl	-176(%rbp), %eax
+	addl	%eax, %edx
+	movl	-172(%rbp), %eax
+	addl	%edx, %eax
+	movq	-24(%rbp), %rsi
+	xorq	%fs:40, %rsi
+	je	.L41
+	call	__stack_chk_fail
+.L41:
+	movq	-8(%rbp), %rbx
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE5:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
